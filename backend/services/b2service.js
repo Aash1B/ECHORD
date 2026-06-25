30cc340b5e1cbdb129feb7589993d3de86e9c0bf
Kritagyaaa <kritagyaarora4782@gmail.com>
2026-06-25 13:28:52 +0530

    added proper B2 code

---
+++ backend/services/b2service.js
@@ -0,0 +1,34 @@
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+require("dotenv").config();
+
+const s3 = new S3Client({
+    endpoint: process.env.B2_ENDPOINT,
+    region: process.env.B2_REGION,
+    credentials: {
+        accessKeyId: process.env.B2_ACCESS_KEY,
+        secretAccessKey: process.env.B2_SECRET_KEY,
+    },
+});
+
+async function getSignedStreamUrl(key) {
+
+    const command = new GetObjectCommand({
+        Bucket: process.env.B2_BUCKET,
+        Key: key,
+    });
+
+    const url = await getSignedUrl(
+        s3,
+        command,
+        {
+            expiresIn: 3600, // 1 hour
+        }
+    );
+
+    return url;
+}
+
+module.exports = {
+    getSignedStreamUrl,
+};
